7dc4358836246624ea8b2cf34f3f36952cfc682b
unknown <ricardoglass@gmail.com>
2019-02-21 22:13:32 +0000

    merge html/css/js, add sucCB, remove bugs

--- js/breakInService.js
+++ js/breakInService.js
@@ -6,34 +6,35 @@ var initialize = function() {
 onDeviceReady = function() {
     setupModal();
     dynamicF();
+    declarevars();
     //StatusBar.styleLightContent(); taken out for web
 }
 
 
 var setupModal = function() {
     var modalWR = document.getElementById('modalWR');
-    var modalWO = document.getElementById('modalWO');
+    //var modalWO = document.getElementById('modalWO');
     var modalWG = document.getElementById('modalWG');
     var modalER = document.getElementById('modalER');
     //var modalEO = document.getElementById('modalEO');
     var modalEG = document.getElementById('modalEG');
 
     var signalWR = document.getElementById('signalWR');
-    var signalWO = document.getElementById('signalWO');
+    //var signalWO = document.getElementById('signalWO');
     var signalWG = document.getElementById('signalWG');
     var signalER = document.getElementById('signalER');
     //var signalEO = document.getElementById('signalEO');
     var signalEG = document.getElementById('signalEG');
 
     var closeWR = document.getElementById("closeWR");
-    var closeWO = document.getElementById("closeWO");
+    //var closeWO = document.getElementById("closeWO");
     var closeWG = document.getElementById("closeWG");
     var closeER = document.getElementById("closeER");
     //var closeEO = document.getElementById("closeEO");
     var closeEG = document.getElementById("closeEG");
 
     var WRForm = document.getElementById("WRForm");
-    var WOForm = document.getElementById("WOForm");
+    //var WOForm = document.getElementById("WOForm");
     var WGForm = document.getElementById("WGForm");
     var ERForm = document.getElementById("ERForm");
     //var EOForm = document.getElementById("EOForm");
@@ -44,7 +45,7 @@ var setupModal = function() {
 
     signalWR.onclick = function() {
         modalWR.style.display = "block";
-        modalWO.style.display = "none";
+        //modalWO.style.display = "none";
         modalWG.style.display = "none";
         modalER.style.display = "none";
         //modalEO.style.display = "none";
@@ -52,7 +53,7 @@ var setupModal = function() {
         behind.style.opacity = "0.5";
 
     }
-    signalWO.onclick = function() {
+    /*signalWO.onclick = function() {
         modalWO.style.display = "block";
         modalWR.style.display = "none";
         modalWG.style.display = "none";
@@ -60,11 +61,11 @@ var setupModal = function() {
         //modalEO.style.display = "none";
         modalEG.style.display = "none";
         behind.style.opacity = "0.5";
-    }
+    }*/
     signalWG.onclick = function() {
         modalWG.style.display = "block";
         modalWR.style.display = "none";
-        modalWO.style.display = "none";
+        //modalWO.style.display = "none";
         modalER.style.display = "none";
         //modalEO.style.display = "none";
         modalEG.style.display = "none";
@@ -73,7 +74,7 @@ var setupModal = function() {
     signalER.onclick = function() {
         modalER.style.display = "block";
         modalWR.style.display = "none";
-        modalWO.style.display = "none";
+        //modalWO.style.display = "none";
         modalWG.style.display = "none";
         //modalEO.style.display = "none";
         modalEG.style.display = "none";
@@ -91,7 +92,7 @@ var setupModal = function() {
     signalEG.onclick = function() {
         modalEG.style.display = "block";
         modalWR.style.display = "none";
-        modalWO.style.display = "none";
+        //modalWO.style.display = "none";
         modalWG.style.display = "none";
         modalER.style.display = "none";
         //modalEO.style.display = "none";
@@ -102,12 +103,13 @@ var setupModal = function() {
         modalWR.style.display = "none";
         behind.style.opacity = "1";
         WRForm.reset();
+        resetModal();
     }
-    closeWO.onclick = function() {
+    /*closeWO.onclick = function() {
         modalWO.style.display = "none";
         behind.style.opacity = "1";
         WOForm.reset();
-    }
+    }*/
     closeWG.onclick = function() {
         modalWG.style.display = "none";
         behind.style.opacity = "1";
@@ -117,6 +119,7 @@ var setupModal = function() {
         modalER.style.display = "none";
         behind.style.opacity = "1";
         ERForm.reset();
+        resetModalER();
     }
     /*closeEO.onclick = function() {
         modalEO.style.display = "none";
@@ -131,81 +134,20 @@ var setupModal = function() {
 
 }
 
-
-// dynamic fields for water, orange
-// also for N/A to empty strings
 var dynamicF = function() {
-    var lowQual = document.getElementById("lowQual"),
-        lowPressure = document.getElementById("lowPressure"),
-        redP = document.getElementById("redP"),
-        redQ = document.getElementById("redQ"),
-        PandQ = document.getElementById("PandQ"),
 
-        endtimeWO = document.getElementById("endtimeWO"),
-        endtimeWR = document.getElementById("endtimeWR"),
-        endtimeEO = document.getElementById("endtimeEO"),
-        endtimeER = document.getElementById("endtimeER"),
-
-        backyetWO = document.getElementById("backyetWO"),
-        backyetWR = document.getElementById("backyetWR"),
-        backyetEO = document.getElementById("backyetEO"),
+    var endtimeER = document.getElementById("endtimeER"),
         backyetER = document.getElementById("backyetER"),
-        backyetWOn = document.getElementById("backyetWOn")
+        backNowER = document.getElementById("backNowER");
 
-        backNowWO = document.getElementById("backNowWO"),
-        backNowWR = document.getElementById("backNowWR"),
-        backNowEO = document.getElementById("backNowEO"),
-        backNowER = document.getElementById("backNowER"),
-
-        municipalWG = document.getElementById("municipalWG"),
-        municipalWO = document.getElementById("municipalWO"),
-        municipalWR = document.getElementById("municipalWR"),
-        /*municipalEG = document.getElementById("municipalEG"),
-        municipalEO = document.getElementById("municipalEO"),
-        municipalER = document.getElementById("municipalER"),*/
-        notMunWG = document.getElementById("notMunWG"),
-        notMunWO = document.getElementById("notMunWO"),
-        notMunWR = document.getElementById("notMunWR");
-        /*notMunEG = document.getElementById("notMunEG"),
-        notMunEO = document.getElementById("notMunEO"),
-        notMunER = document.getElementById("notMunER");*/
 }
 
 function handleChange() {
-    if (redP.checked) {
-        lowPressure.style.display = "block";
-        lowQual.style.display = "none";
-    }
-    else if (redQ.checked) {
-        lowQual.style.display = "block";
-        lowPressure.style.display = "none";
-    }
-    else if (PandQ.checked) { //why is this blue?
-        lowQual.style.display = "block";
-        lowPressure.style.display = "block";
-    }
-
-    if (backyetWO.checked) {
-        backNowWO.style.display = "block";
-        lowPressure.style.display = "none";
-    }
-    else if (backyetWOn.checked) {
-        backNowWO.style.display = "none";
-        //lowPressure.style.display = "block"; //not necessary here
-    }
-
     if (backyetER.checked) {
         backNowER.style.display = "block";
     }
     else {
         backNowER.style.display = "none";
-    }
-
-    if (backyetWR.checked) {
-        backNowWR.style.display = "block";
-    }
-    else {
-        backNowWR.style.display = "none";
     }
 
     if (backyetEO.checked) {
@@ -214,162 +156,167 @@ function handleChange() {
     else {
         backNowEO.style.display = "none";
     }
-
-//sorting municipal questions
-    if (municipalWG.checked) {
-        notMunWG.style.display = "none";
-        sourceWG.value = "n/a";
-    }
-    else {
-        notMunWG.style.display = "block";
-        sourceWG.value = "";
-    }
-    if (municipalWO.checked) {
-        notMunWO.style.display = "none";
-        sourceWO.value = "n/a";
-    }
-    else {
-        notMunWO.style.display = "block";
-        sourceWO.value = "";
-    }
-    if (municipalWR.checked) {
-        notMunWR.style.display = "none";
-        sourceWR.value = "n/a";
-    }
-    else {
-        notMunWR.style.display = "block";
-        sourceWR.value = "";
-    }
-
 }
 
-function formSubmit(event, formID, modalID, sucCB) {
-    event.preventDefault();
+//declare all vars relating to water, red
+var declarevars = function() {
 
+    var userInfo = document.getElementById('userInfo');
+    var intro = document.getElementById('intro');
+    var next = document.getElementById('next');
+    var typeO = document.getElementById('typeO');
+    var condO = document.getElementById('condO');
+    var sendWR = document.getElementById('sendWR');
+    var loadNext = document.getElementById('loadNext');
+    var waterCutY = document.getElementById('waterCutY');
+    var waterCutN = document.getElementById('waterCutN');
+    var reductionPressure = document.getElementById('reductionPressure');
+    var reductionQuality = document.getElementById('reductionQuality');
+    var taste = document.getElementById('taste');
+    var smell = document.getElementById('smell');
+    var appearance = document.getElementById('appearance');
+
+    var backyetY = document.getElementById('backyetY');
+    var backyetN = document.getElementById('backyetN');
+
+    var timeBack = document.getElementById('timeBack');
+
+    //minor addition for power, red
+    var timebackQER = document.getElementById('timebackQER');
+    var timebackER = document.getElementById('timebackER');
+    var backyetERY = document.getElementById('backyetERY');
+    var backyetERN = document.getElementById('backyetERN');
+    var starttimeER = document.getElementById('starttimeER');
+
+    //determines what questions will be asked on page 2
+    loadNext.onclick = function () {
+        userInfo.style.display = "none";
+        intro.style.display = "none";
+        next.style.display = "block";
+        if (reductionQuality.checked) {
+            condO.style.display = "block";
+        }
+        else {
+            condO.style.display = "none";
+        }
+    }
+}
+
+//ensures smartfields reset on close
+function resetModal() {
+    loadNext.style.display = "none";
+    condO.style.display = "none";
+    typeO.style.display = "none";
+    next.style.display = "none";
+    sendWR.style.display = "none";
+    userInfo.style.display = "block";
+    intro.style.display = "block";
+}
+
+function resetModalER() {
+    timebackQER.style.display = "none";
+    sendER.style.display = "none";
+}
+
+//smart fields for page 1
+function smartFormIntro() {
+    if (waterCutY.checked) {
+        typeO.style.display = "none";
+        reductionPressure.checked = false;
+        reductionQuality.checked = false;
+        loadNext.style.display = "block";
+    }
+    else if (waterCutN.checked) {
+        typeO.style.display = "block";
+        if ( (reductionPressure.checked) || (reductionQuality.checked) ) {
+            loadNext.style.display = "block";
+        }
+        else {
+            loadNext.style.display = "none";
+        }
+    }
+
+
+    //minor addition for power, red
+    if (backyetERY.checked) {
+        timebackQER.style.display = "block";
+    }
+    else if (backyetERN.checked) {
+        timebackQER.style.display = "none";
+        timebackER.valueAsDate = null;
+    }
+
+    if (starttimeER.valueAsDate != null) {
+        if ((backyetERN.checked) || (backyetERY.checked && timebackER.valueAsDate != null)) {
+            sendER.style.display = "block";
+        }
+        else sendER.style.display = "none";
+    }
+    else sendER.style.display = "none";
+}
+
+//smart fields for page 2, including submit button
+function smartForm() {
+    if (backyetY.checked) {
+        timeBackQ.style.display = "block";
+    }
+    else if (backyetN.checked) {
+        timeBackQ.style.display = "none";
+        timeBack.valueAsDate = null;
+    }
+    if ( (((condO.style.display) == "block") && (taste.checked || smell.checked || appearance.checked) ) || ((condO.style.display) == "none")) {
+        if (startTime.valueAsDate != null) {
+            if ((backyetY.checked) && (timeBack.valueAsDate != null)) {
+                sendWR.style.display = "block";
+            }
+            else if (backyetN.checked) {
+                sendWR.style.display = "block";
+            }
+            else {
+                sendWR.style.display = "none";
+            }
+        }
+        else {
+            sendWR.style.display = "none";
+        }
+    }
+    else {
+        sendWR.style.display = "none";
+    }
+}
+
+
+
+//converts data collected from the form
+function formSubmit(event, formID, modalID, sucCB, sendB) {
+    event.preventDefault();
     
     var form = $('#'+formID);
     var id = document.getElementById(formID).ID;
     id.value = Math.floor((Math.random() * 100000000) + 1);
     var timestamp = document.getElementById(formID).timestamp;
     timestamp.value = Date();
-    /*var firstName = document.getElementById(formID).firstName;
-    var lastName = document.getElementById(formID).lastName;
-    var address = document.getElementById(formID).address;
-    var postcode = document.getElementById(formID).postcode;
-    var country = document.getElementById(formID).country; */    //removed for web app
-
-    if ((redP.checked) && (timeTaken.value=="") && (backyetWOn.checked)) {
-        timeTaken.value = "not given";
-    }
-    if ((PandQ.checked) && (timeTaken.value=="") && (backyetWOn.checked)) {
-        timeTaken.value = "not given";
-    }
-    if ( ((redQ.checked) || (PandQ.checked)) && (descRedQ.value=="") ) { //this should work, need to properly test
-        descRedQ.value = "not given";
-    }
-
-    /* not possible for web version
-    if (file.entry) {
-        file.entry.file(function (detailsFile) {
-            var reader = new FileReader();
-            reader.onloadend = function (evt) {
-                var textArray = evt.target.result.split("\n");
-                firstName.value = textArray[0];
-                lastName.value = textArray[1];
-                address.value = textArray[2];
-                postcode.value = textArray[3];
-                country.value = textArray[4];
-                formSend(form);
-                return true;
-            }
-            reader.readAsText(detailsFile);
-
-        }, failCB("FileReader"));
-    }
-    //return false;
-    */
-
-    /*firstName.value = "firsttest";
-    lastName.value = "lasttest";
-    address.value = 'addtest';
-    postcode.value = "posttest";
-    country.value = "countrytest";*/
 
 
-
-    //changing all zero string values to "N/A"
-    //for WR
-    if (modalWR.style.display == "block") {
-        if (backNowWR.style.display == "none") {
-            endtimeWR.value = "N/A";
-        }
-    }
-
-    //for WO
-    if (modalWO.style.display == "block") {
-
-        if (lowPressure.style.display == "none") {
-            timeTaken.value = "N/A";
-        }
-        if (lowQual.style.display == "none") { //won't work?
-            tasteB.value = "N/A";
-            appearanceB.value = "N/A";
-            smellB.value = "N/A";
-            descRedQ.value = "N/A";
-        }
-        if (backNowWO.style.display == "none") {
-            endtimeWO.value = "N/A";
-        }
-    }
-
-    //for ER
-    if (modalER.style.display == "block") {
-
-        if (backNowER.style.display == "none") {
-            endtimeER.value = "N/A";
-        }
-    }
-
-    //for EO
-    if (modalEO.style.display == "block") {
-
-        if (backNowEO.style.display == "none") {
-            endtimeEO.value = "N/A";
-        }
-    }
-
-    //for municipal
-    if (modalWG.style.display == "block") {
-        if (notMunWG.style.display == "none") {
-            sourceWG.value = "municipal";
-        }
-    }
-    if (modalWO.style.display == "block") {
-        if (notMunWO.style.display == "none") {
-            sourceWO.value = "municipal";
-        }
-    }
-    if (modalWR.style.display == "block") {
-        if (notMunWR.style.display == "none") {
-            sourceWR.value = "municipal";
-        }
-    }
+    //remove all inputs with no values
+    form.find(":input").filter(function(){ return !this.value; }).attr("disabled", "disabled");
 
     formSend(form); //added for web
 
     /*var modal = document.getElementById(modalID);
-    modal.style.display = "none";*/
-
+    modal.style.display = "none"; 
     var sucCB = document.getElementById(sucCB);
-
     sucCB.innerHTML = "Thank you!";
     sucCB.style.color = "green";
-
-
-
+    */
+    var sucCB = document.getElementById(sucCB);
+    var sendB = document.getElementById(sendB);
+    sendB.style.display = "none";
+    sucCB.style.display = "block";
 
 }
 
+//sends converted data to server through ajax
 function formSend(form) {
     $.ajax({
        type: "POST",
