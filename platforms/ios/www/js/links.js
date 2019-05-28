74fd000a248874c8fd2cf8ce689d180862bfb6b1
Richard Glass <Richard@Richards-MacBook-Pro.local>
2019-05-28 15:41:36 +0100

    add facebook link image, fully working build

--- platforms/ios/www/js/links.js
+++ platforms/ios/www/js/links.js
@@ -5,3 +5,7 @@ var openWeb = () => {
 const openQ = () => {
 	cordova.InAppBrowser.open('https://imperial.eu.qualtrics.com/jfe/form/SV_0NUlFWS9PTRowxT', '_system', 'location=no');
 }
+
+const openFacebook = () => {
+	cordova.InAppBrowser.open('https://www.facebook.com/NetworkingWater/', '_system', 'location=no');
+}
