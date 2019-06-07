4c1d12d6d3ef3a8f33d1bd1f00ca402ead3cd8e1
unknown <ricardoglass@gmail.com>
2019-06-07 16:20:40 +0100

    change text and logic for v2.0.3, release build

--- js/breakInService.js
+++ js/breakInService.js
@@ -39,12 +39,12 @@ var setupModal = function() {
         modalWR.style.display = "none";
         behind.style.opacity = "1";
         WRForm.reset();
-        resetModal();
     }
     closeWG.onclick = function() {
         modalWG.style.display = "none";
         behind.style.opacity = "1";
         WGForm.reset();
+        resetModal();
     }
 }
 
@@ -56,6 +56,7 @@ var declarevars = function() {
         next = document.getElementById('next');
         typeO = document.getElementById('typeO');
         condO = document.getElementById('condO');
+        sendWG = document.getElementById('sendWG'); //added 7/06
         sendWR = document.getElementById('sendWR');
         loadNext = document.getElementById('loadNext');
         waterCutY = document.getElementById('waterCutY');
@@ -63,8 +64,8 @@ var declarevars = function() {
         reductionPressure = document.getElementById('reductionPressure');
         reductionQuality = document.getElementById('reductionQuality');
         taste = document.getElementById('taste');
-        smell = document.getElementById('smell');
-        appearance = document.getElementById('appearance');
+        odour = document.getElementById('odour');
+        colour = document.getElementById('colour');
 
         backyetY = document.getElementById('backyetY');
         backyetN = document.getElementById('backyetN');
@@ -76,111 +77,41 @@ var declarevars = function() {
         //userInfo.style.display = "none";
         intro.style.display = "none";
         next.style.display = "block";
-        if (reductionQuality.checked) {
-            condO.style.display = "block";
-        }
-        else {
-            condO.style.display = "none";
-        }
     }
 }
 
 //ensures smartfields reset on close
 function resetModal() {
     loadNext.style.display = "none";
-    condO.style.display = "none";
-    typeO.style.display = "none";
     next.style.display = "none";
-    sendWR.style.display = "none";
+    //timeBackQ.style.display = "none";
+    sendWG.style.display = "none";
     intro.style.display = "block";
 }
 
 //smart fields for page 1
 function smartFormIntro() {
-    if (waterCutY.checked) {
-        typeO.style.display = "none";
-        reductionPressure.checked = false;
-        reductionQuality.checked = false;
+    if ((timeBack.valueAsDate != null) && (startTime.valueAsDate != null)) {
         loadNext.style.display = "block";
-    }
-    else if (waterCutN.checked) {
-        typeO.style.display = "block";
-        if ( (reductionPressure.checked) || (reductionQuality.checked) ) {
-            loadNext.style.display = "block";
-        }
-        else {
-            loadNext.style.display = "none";
-        }
+    } else {
+        loadNext.style.display = "none";
     }
 }
 
 //smart fields for page 2, including submit button
 function smartForm() {
-    if (backyetY.checked) {
-        timeBackQ.style.display = "block";
-    }
-    else if (backyetN.checked) {
-        timeBackQ.style.display = "none";
-        timeBack.valueAsDate = null;
-    }
-    if ( (((condO.style.display) == "block") && (taste.checked || smell.checked || appearance.checked) ) || ((condO.style.display) == "none")) {
-        if (startTime.valueAsDate != null) {
-            if ((backyetY.checked) && (timeBack.valueAsDate != null)) {
-                sendWR.style.display = "block";
+    if ( colourN.checked || colourY.checked ) {
+        if ( odourN.checked || odourY.checked ) {
+            if ( tasteN.checked || tasteY.checked ) {
+                sendWG.style.display = "block";
+            } else {
+                sendWG.style.display = "none";
             }
-            else if (backyetN.checked) {
-                sendWR.style.display = "block";
-            }
-            else {
-                sendWR.style.display = "none";
-            }
+        } else {
+            sendWG.style.display = "none";
         }
-        else {
-            sendWR.style.display = "none";
-        }
+    } else {
+        sendWG.style.display = "none";
     }
-    else {
-        sendWR.style.display = "none";
-    }
+
 }
-
-
-/*
-//converts data collected from the form
-function formSubmit(event, formID, modalID, sucCB, sendB) {
-    event.preventDefault();
-    
-    var form = $('#'+formID);
-    var id = document.getElementById(formID).ID;
-    id.value = Math.floor((Math.random() * 100000000) + 1);
-    var timestamp = document.getElementById(formID).timestamp;
-    timestamp.value = Date();
-
-
-    //remove all inputs with no values
-    form.find(":input").filter(function(){ return !this.value; }).attr("disabled", "disabled");
-
-    formSend(form); //added for web
-
-    var sucCB = document.getElementById(sucCB);
-        sendB = document.getElementById(sendB);
-    sendB.style.display = "none";
-    sucCB.style.display = "block";
-}
-
-//sends converted data to server through ajax
-function formSend(form) {
-    $.ajax({
-     type: "POST",
-     url: "https://xdfz4o3cjk.execute-api.eu-west-2.amazonaws.com/live/networkingwater",
-     data: form.serialize(),
-     dataType: 'x-www-form-urlencoded',
-     contentType: 'application/x-www-form-urlencoded',
-     success: function(data)
-     {
-         alert("Data Submitted!");
-         console.log('submit successful')
-     }
- });
-}
-*/
